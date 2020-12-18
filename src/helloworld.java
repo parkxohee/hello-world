dae4d4e23a965b5dbce1e1e26167a5ac30921de7
xohee <xohee@xohee-pc>
2020-12-19 01:16:56 +0900

    메시지 추가_Hi

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		System.out.println("¾È³ç¼¼°è¾ß");
+		System.out.println("Hi, world!");
 	}
 }
